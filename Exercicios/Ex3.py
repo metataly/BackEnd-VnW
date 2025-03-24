4a245f4c60ade1bcc60d18d50c8e59d3676a2178
Nathaly <167140283+metataly@users.noreply.github.com>
2025-03-24 19:13:23 -0300

    Update Ex3.py

--- Exercicios/Ex3.py
+++ Exercicios/Ex3.py
@@ -4,7 +4,7 @@ Exemplo: [‘Java’, ‘Java’, ‘Ruby’, ‘Javascript’, ‘Ruby’]
 Saída: {‘Java’: 2, ‘Ruby’: 2, ‘Javascript’: 1} '''
 
 def qtd_elemento(elem):
-
+    lista = elem.split(' ')
     dicionario = {}
     for i in elem:
         dicionario[i] = elem
@@ -13,7 +13,7 @@ def qtd_elemento(elem):
 
 def main():
     
-    elementos =  ['Java', 'Java', 'Ruby', 'Javascript', 'Ruby', 'Ruby']
+    elementos =  "Java Java Ruby Javascript Ruby Ruby"
     
     resp = qtd_elemento(elementos)
     print(resp)
